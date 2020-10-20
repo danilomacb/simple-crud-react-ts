91f2debf14a6d1f03bccf95af6428666f4cc668d
danilomacb <danilo.bakun@gmail.com>
2020-10-20 15:49:17 -0300

    taking the old project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Form, InputGroup, Button } from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const url = "http://localhost:3001/";
+
+  interface IElement {
+    _id?: string;
+    content?: string;
+  }
+
+  const [elements, setElements] = useState<IElement[]>([]);
+
+  let createInput: HTMLInputElement;
+
+  useEffect(() => {
+    read();
+  }, []);
+
+  const read = async () => {
+    try {
+      const response = await fetch(url);
+      const elements: IElement[] = await response.json();
+
+      return setElements(elements);
+    } catch (err) {
+      alert("Error on read elements");
+      return console.error("Error on read elements: ", err);
+    }
+  };
+
+  const create = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: createInput.value }),
+      });
+
+      createInput.value = "";
+
+      read();
+    } catch (err) {
+      alert("Error on create element");
+      return console.error("Error on create element: ", err);
+    }
+  };
+
+  const update = (id?: string) => async (
+    event: React.KeyboardEvent & React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      try {
+        const response = await fetch(url + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ content: event.target.value }),
+        });
+
+        if (response.ok) alert("Update succeeded");
+      } catch (err) {
+        alert("Error on update element");
+        return console.error("Error on update element: ", err);
+      }
+    }
+  };
+
+  const del = async (id?: string) => {
+    try {
+      await fetch(url + id, { method: "DELETE" });
+
+      read();
+    } catch (err) {
+      alert("Error on delete element");
+      return console.error("Error on delete element: ", err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Row>
+        {elements.map((element) => (
+          <Col key={element._id} sm={6} md={4} lg={3} className="p-2">
+            <InputGroup>
+              <InputGroup.Prepend>
+                <Button variant={"danger"} onClick={() => del(element._id)}>X</Button>
+              </InputGroup.Prepend>
+              <Form.Control type="text" defaultValue={element.content} onKeyDown={update(element._id)} />
+            </InputGroup>
+          </Col>
+        ))}
+        <Col sm={6} md={4} lg={3} className="p-2">
+          <Form onSubmit={create}>
+            <Form.Control
+              type="text"
+              placeholder="New"
+              ref={(input: HTMLInputElement) => {
+                createInput = input;
+              }}
+            />
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
